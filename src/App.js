831e42ba97f10e0d698ce76494a4e86fcba7da06
Zgene <Zburdge4389@gmail.com>
2023-06-13 13:15:32 -0700

    Added Dictionary component/Search Engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from "./logo.svg";
+import Dictionary from "./Dictionary.js";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid mb-5" alt="logo" />
+        </header>
+        <Dictionary />
+        <footer className="text-center mt-5">Coded by Zgene' Burdge</footer>
+      </div>
     </div>
   );
 }
