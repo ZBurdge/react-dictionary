0de01cd1525bdfef8b3f44c5f3b199840ef3dcae
Zgene <Zburdge4389@gmail.com>
2023-06-14 15:04:38 -0700

    Added h1 to header

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo img-fluid mb-5" alt="logo" />
+        <header className="App-header mb-5">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <h1 className="text-center">Z's Dictionary</h1>
         </header>
         <Dictionary />
         <footer className="text-center mt-5">Coded by Zgene' Burdge</footer>
