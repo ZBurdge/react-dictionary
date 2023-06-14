3fc0d4e6ff098911ac176629af7da8af47d266ac
Zgene <Zburdge4389@gmail.com>
2023-06-14 13:00:32 -0700

    Changed API host

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -12,7 +12,8 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
 
-    const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    const apiKey = "354625fc8tco3a0bc76af830b102d699";
+    const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
